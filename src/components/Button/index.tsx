2dd14ad494cc8eaceee0d1bf208b165702c50b20
Paulo Lemes <paulo-lemes@live.com>
2024-07-09 23:54:14 -0300

    Updated button style options

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,16 +3,22 @@ import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentProps<"button"> {
   children: ReactNode;
+  primaryStyle?: boolean;
 }
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, primaryStyle, ...props }: ButtonProps) {
+  const btnBg = primaryStyle ? "primary-400" : "neutral-800";
+  const btnText = primaryStyle ? "neutral-950" : "neutral-200";
+
   return (
     <button
       {...props}
       className={twMerge(
-        "flex gap-2 px-2.5 py-2 sm:px-5 rounded-lg shadow-shape bg-primary-400 text-neutral-950 transition-all hover:bg-primary-500 active:scale-95",
+        `flex-center min-w-max gap-2 px-2.5 py-2 sm:px-5 rounded-lg shadow-shape transition-all bg-${btnBg} text-${btnText}`,
         props.className,
-        props.disabled ? "" : ""
+        props.disabled
+          ? `bg-${btnBg}/60`
+          : `${primaryStyle ? "hover:bg-primary-500" : "hover:bg-neutral-700"} active:scale-95`
       )}
     >
       {children}
