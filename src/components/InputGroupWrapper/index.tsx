492f716259266411f02e9b70323b6e5e1867845e
Paulo Lemes <paulo-lemes@live.com>
2024-07-12 00:37:12 -0300

    Created TripLocationAndDates component

--- src/components/InputGroupWrapper/index.tsx
+++ src/components/InputGroupWrapper/index.tsx
@@ -1,12 +1,22 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface InputGroupWrapperProps {
   children: ReactNode;
+  classCSS?: string;
 }
 
-export function InputGroupWrapper({ children }: InputGroupWrapperProps) {
+export function InputGroupWrapper({
+  children,
+  classCSS = "",
+}: InputGroupWrapperProps) {
   return (
-    <div className="w-[760px] max-w-full min-h-16 py-3 px-6 rounded-xl shadow-shape flex flex-wrap gap-4 sm:gap-2 bg-neutral-900 items-stretch">
+    <div
+      className={twMerge(
+        "w-[760px] max-w-full min-h-16 py-3 px-6 rounded-xl shadow-shape flex flex-wrap gap-4 sm:gap-2 bg-neutral-900 items-stretch",
+        classCSS
+      )}
+    >
       {children}
     </div>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,13 @@
-export const iconStyle = "size-5"
-export const inputIconStyle = "size-5 text-neutral-300"
+import { format } from "date-fns";
+import { DateRange } from "react-day-picker";
+
+export const iconStyle = "size-5";
+export const inputIconStyle = "size-5 text-neutral-300";
+
+export function formatDisplayedDate(dates: DateRange | undefined) {
+  return dates && dates.from && dates.to
+    ? format(dates.from, "d' de 'LLL")
+        .concat(" até ")
+        .concat(format(dates.to, "d' de 'LLL"))
+    : null;
+}
