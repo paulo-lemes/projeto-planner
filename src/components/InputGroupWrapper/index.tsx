3dfaedd82026f4a2edd350e8f24470235ae45a0e
Paulo Lemes <paulo-lemes@live.com>
2024-07-12 01:41:38 -0300

    Created Activities component

--- src/components/InputGroupWrapper/index.tsx
+++ src/components/InputGroupWrapper/index.tsx
@@ -11,13 +11,13 @@ export function InputGroupWrapper({
   classCSS = "",
 }: InputGroupWrapperProps) {
   return (
-    <div
+    <section
       className={twMerge(
         "w-[760px] max-w-full min-h-16 py-3 px-6 rounded-xl shadow-shape flex flex-wrap gap-4 sm:gap-2 bg-neutral-900 items-stretch",
         classCSS
       )}
     >
       {children}
-    </div>
+    </section>
   );
 }
