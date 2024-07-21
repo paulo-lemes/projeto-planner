c1a975ae2f57405c9ef62a9f63b1c54d8f9c217d
Paulo Lemes <paulo-lemes@live.com>
2024-07-21 00:39:41 -0300

    Added invalid destination test

--- cypress/e2e/update-trip.cy.ts
+++ cypress/e2e/update-trip.cy.ts
@@ -1,12 +1,28 @@
 describe("Update trip spec", () => {
   beforeEach(() => {
     cy.visitTripDetailsPage();
+    cy.wait(1000);
   });
 
   it("should update trip details", () => {
     cy.getByData("update-trip-details-button").click();
+    cy.getByData("destination-input").clear();
     cy.getByData("destination-input").type("Los Angeles - CA");
     cy.getByData("confirm-change-trip-details-button").click();
     cy.verifyDialogTextAndClose("Informações alteradas com sucesso!");
+  });
+
+  it("should not update trip details with destination empty or short", () => {
+    cy.getByData("update-trip-details-button").click();
+    cy.getByData("destination-input").clear();
+    cy.getByData("confirm-change-trip-details-button").click();
+    cy.verifyDialogTextAndClose(
+      "Erro ao processar a alteração: informação de destino deve conter pelo menos 4 caracteres"
+    );
+    cy.getByData("destination-input").type("123");
+    cy.getByData("confirm-change-trip-details-button").click();
+    cy.verifyDialogTextAndClose(
+      "Erro ao processar a alteração: informação de destino deve conter pelo menos 4 caracteres"
+    );
   });
 });
