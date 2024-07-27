bf25c06eb9d4d6f1a0f5557b6227441d05ed3184
Paulo Lemes <paulo-lemes@live.com>
2024-07-26 23:14:02 -0300

    Updated types, added common file

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -10,26 +10,7 @@ export type {
   TripLocationAndDatesProps,
 } from "./components-props";
 
-export interface Activity {
-  date: string;
-  activities: {
-    id: string;
-    title: string;
-    occurs_at: string;
-  }[];
-}
-
-export interface Link {
-  id: string;
-  title: string;
-  url: string;
-}
-export interface Participant {
-  id: string;
-  name: string;
-  email: string;
-  is_confirmed: boolean;
-}
+export type { Activity, Link, Participant } from "./common";
 
 export interface DialogContextType {
   isDialogOpen: boolean;
