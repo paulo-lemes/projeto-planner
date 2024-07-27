9845e1bd438021793966334a6e09178b55c33715
Paulo Lemes <paulo-lemes@live.com>
2024-07-26 23:19:21 -0300

    Updated types exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,3 @@
-export type {
-  ActivitiesProps,
-  ImportantLinksProps,
-  InputGroupWrapperProps,
-  InputModalWrapperProps,
-  InviteGuestsProps,
-  LocationAndDatesGroupProps,
-  ModalProps,
-  ParticipantsProps,
-  TripLocationAndDatesProps,
-} from "./components-props";
-
-export type { Activity, Link, Participant } from "./common";
-
-export type { DialogContextType } from "./context";
+export * from "./common";
+export * from "./components-props";
+export * from "./context";
